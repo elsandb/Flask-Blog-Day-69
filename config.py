8ab0080023430310ebd04ef32ea620d5afdaa883
Elisabeth <elisabeth.sandbakken@gmail.com>
2023-04-27 15:18:50 +0200

    Set SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
