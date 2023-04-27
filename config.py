9d2d077fcf52d04f14a54bb5b8e95f05323304af
Elisabeth <elisabeth.sandbakken@gmail.com>
2023-04-27 16:20:50 +0200

    Change sqlite to postgres

--- config.py
+++ config.py
@@ -11,13 +11,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     """Set Flask configuration variables."""
     # General config
-    ENVIRONMENT = os.environ.get('ENVIRONMENT')
     FLASK_DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
