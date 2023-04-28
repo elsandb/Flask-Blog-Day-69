4ea93a4b5a598bd83986ecce75fb10d6846b0fc8
Elisabeth <elisabeth.sandbakken@gmail.com>
2023-04-28 11:18:10 +0200

    Try Render

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
